077bf056a56006aa2b0c05bc1f0da8f2ea2bcae5
Yun Sung Choi <cyss0317@gmail.com>
2021-07-30 16:58:58 -0500

    React is working

--- frontend/sunny.jsx
+++ frontend/sunny.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import configureStore from './store/store'
 
-document.addEventListener("DOMContentloaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root")
 
     ReactDOM.render( <h1>react is working</h1>, root )
