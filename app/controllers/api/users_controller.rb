667e497cce4cb7b7006ce6016d4c4653895f3740
Yun Sung Choi <cyss0317@gmail.com>
2021-07-30 19:11:57 -0500

    Work on jbuilder

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,11 @@
-class UsersController < ApplicationController
+class Api::UsersController < ApplicationController
 
     def create
         @user = User.new
         if @user.save!
             login!(@user)
-            render "api/users/show"
+            render :show
+            # render json: :show
         else
             render json: @user.errors.full_messages, status: 422
         end
