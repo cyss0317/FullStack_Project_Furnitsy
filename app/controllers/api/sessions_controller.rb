01bc2454c49ad06bc4dfd47deecd1dbe81d43aa6
Yun Sung Choi <cyss0317@gmail.com>
2021-07-31 17:40:22 -0500

    Add render json for sessions_controlle.rb

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,24 @@
-class SessionsController < ApplicationController
+class Api::SessionsController < ApplicationController
+    require
+    def create
+        @user = User.find_by_credential(params[:user][:username], params[:user][:password])
+
+        if @user
+            login!(@user)
+            render "api/users"
+        else
+            render json: ["Invalid username or password"], status: 401
+        end
+    end
+
+    def destroy
+        @user = current_user
+        if @user
+            logout!
+            render  "api/users"
+        else
+            render json: ["Successfully logged out"]
+        end
+
+    end
 end
