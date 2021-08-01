53ef41590a5de8fb60a69ef6b10104ca8d05fb49
Yun Sung Choi <cyss0317@gmail.com>
2021-07-31 22:27:01 -0500

    Test ajax request function on console

--- frontend/sunny.jsx
+++ frontend/sunny.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { $login, $logout } from "./util/session_api_util"
 // import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root")
 
+    window.login = $login;
+    window.logout = $logout;
     ReactDOM.render( <h1>react is working</h1>, root )
+
+
+
 })
