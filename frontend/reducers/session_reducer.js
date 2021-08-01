c7f55dc0ae60ed8421f5686e93cd86ca58e979e7
Yun Sung Choi <cyss0317@gmail.com>
2021-08-01 09:34:15 -0500

    Add session_reducer, users_reducer, entities_reducer.

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,20 @@
+//takes care of the actions and returns a correspoding value
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
+
+const SessionReducer = (oldState, action) => {
+    Object.freeze(oldState);
+
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return { id: action.currentUser.id};
+        case LOGOUT_CURRENT_USER:
+
+            const nextState = Object.assign({}, oldState, { [id]: null})
+            // const nextState = Object.freeze({ id: null})
+            return nextState;
+        default:
+            return oldState;
+    }
+}
+
+export default SessionReducer;
