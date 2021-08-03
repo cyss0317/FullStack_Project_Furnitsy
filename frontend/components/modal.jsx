37c9a8ae122b302c9131765a0cabed054d74aff7
Yun Sung Choi <cyss0317@gmail.com>
2021-08-03 08:04:31 -0500

    Add modal_container.js and modal.jsx, Still working on modal.jsx

---
+++ frontend/components/modal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginFormContainer from "./login_form_container";
+import GreetingContainer from "./greeting_container";
+
+
+//if state.ui.modal is not null, render modal???
+const Modal = ({ modal, closeModal }) => {
+    
+}
+
+
+
+export default Modal;
--- frontend/reducers/modal_reducer.js
+++ frontend/reducers/modal_reducer.js
@@ -1,11 +1,13 @@
 import { OPEN_MODAL, CLOSE_MODAL} from "../actions/modal_actions";
 
 const ModalReducer= (oldState = null, action){
+    Object.freeze(oldState);
+
     switch (action.type) {
         case OPEN_MODAL:
-            
+            return action.modal;
         case CLOSE_MODAL:
-    
+            return null;
         default:
             return oldState;
     }
