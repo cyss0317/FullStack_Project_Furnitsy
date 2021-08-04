42bf5ce462558e13d771f75ab7e3cbed8f741c17
Yun Sung Choi <cyss0317@gmail.com>
2021-08-04 04:22:57 -0500

    Adding database for couch and sofa to seeds.rb

--- db/seeds/01_couches.rb
+++ db/seeds/01_couches.rb
@@ -1,4 +1,4 @@
-Product.create({name: "Belcampo sofa", description: "The sumptuously comfortable Belcampo sofa makes it easy to camp out in style. Inspired by quality menswear, the linen-weave upholstery naturally works in a rich jute tone. Crisply tailored box cushions and roll arms give the sofa a modern classic shape sure to complement so many aesthetics. Sporting shades of warm terracotta and indigo blue, four decorative pillows add fashionable flair.
+Product.create(name: "Belcampo Sofa", description: "The sumptuously comfortable Belcampo sofa makes it easy to camp out in style. Inspired by quality menswear, the linen-weave upholstery naturally works in a rich jute tone. Crisply tailored box cushions and roll arms give the sofa a modern classic shape sure to complement so many aesthetics. Sporting shades of warm terracotta and indigo blue, four decorative pillows add fashionable flair.
 Corner-blocked frame
 Attached back and loose seat cushions
 High-resiliency foam cushions wrapped in thick poly fiber
@@ -10,4 +10,83 @@ Excluded from promotional discounts and coupons
 Fabric Details
 Body and One Side Toss Pillows: Polyester (100)%
 Oneside Toss Pillows: Polyester (100)%
-140 lbs. (63.5 kgs.)", category: "couch_and_sofa", measurement: 'Width: 102.00", Depth: 41.00", Height: 40.00"', color: "gray", price: 899.00})
+140 lbs. (63.5 kgs.)", category: "Couch and Sofa", measurement: 'Width: 102.00", Depth: 41.00", Height: 40.00"', color: "gray", price: 899.00)
+
+Product.create({name: "Abinger Sofa", description: "If you’re looking to lighten the mood, you’re sure to find the space-conscious, budget-conscious Abinger sofa a breath of fresh air. Covered in a cream-tone chenille upholstery loaded with plush texture, this less-is-more contemporary sofa is dressed to impress with angled side profiling and track armrests wrapped with a layer of pillowy softness for that little something extra. Crisp and clean, the 2-over-2 cushion styling adds up to one sensational look.
+Corner-blocked frame
+Attached back and loose seat cushions
+High-resiliency foam cushions wrapped in thick poly fiber
+Polyester upholstery
+2 throw pillows included
+Pillows with soft polyfill
+Exposed feet with faux wood finish
+Fabric Details
+Body and One Side Toss Pillows: Polyester (100)%
+Oneside Toss Pillows: Polyester (100)%
+Weight
+124 lbs. (56.25 kgs.)", category: "Couch and Sofa", measurement: '"Width: 89.00", Depth: 38.00", Height: 39.00"' ,color: "Natural and Smoke", price: 649.00 })
+# question1. what if there are two color options, should that be type "array?"
+# Couch and Sofa
+# t.string :color, array: true, default: []
+Product.create({name: "Lyman Sofa", description: "Description
+Mixing mid-century inspiration with modern attitude, the Lyman sofa is as retro chic as it is fashion forward. Loaded with multi-tonal interest, the granite gray upholstery is the essence of earthy elegance. Design elements include 2-over-2 cushion styling, t-cushions wrapping around slim track arms and tapered accent legs that are right on point.
+Corner-blocked frame
+Attached back and loose seat cushions
+High-resiliency foam cushions wrapped in thick poly fiber
+Polyester upholstery
+Exposed feet with faux wood finish
+Platform foundation system resists sagging 3x better than spring system after 20,000 testing cycles by providing more even support
+Smooth platform foundation maintains tight, wrinkle-free look without dips or sags that can occur over time with sinuous spring foundations
+Fabric Details
+Body: Polyester (100)%
+Weight
+103 lbs. (46.72 kgs.)", category: "Couch and Sofa", measurement: 'Width: 82.00"Depth: 40.00"Height: 36.00"', color: "Graphite", price: 749.00 })
+
+Product.create({name: "Gaddis Flip Flop Sofa", description: "Love furniture with a mid-century modern twist? Then you’re sure to flip for the gorgeous Gaddis flip flop sofa in gray. This brilliantly styled sofa-futon is the ultimate choice if you’re short on space but long for form and function. Tailored touches including tapered wood-tone legs and tufted box cushions exude an upscale mood reflecting your fine taste.
+Attached back and seat cushions
+High-resiliency foam cushions wrapped in thick poly fiber
+Polyester upholstery
+Tufted details
+2 USB ports; power cord included; UL Listed
+Exposed wood-tone legs
+Estimated Assembly Time: 30 Minutes
+Fabric Details
+: Polyester (100)%
+Weight
+111 lbs. (50.35 kgs.)", category: "Couch and Sofa", measurement: 'Width: 78.00"Depth: 32.00"Height: 32.00"', color: "Gray", price: 599.00 })
+
+Product.create({name: "Saveto 3-Piece Modular Sofa", description: "Looking comfortable. Feeling comfortable. That’s what modern farmhouse living is all about. What better way to embrace the modern farmhouse trend than with the Savesto 3-piece sectional sofa in ivory white. As casually cool as they are luxurious, the sectional’s reversible back and seat cushions are feather filled and wrapped in a cottony soft fabric for cloud-like comfort. Crisp, clean lines and track arm styling simply work.
+Includes 3 pieces: armless chair, left-arm facing corner chair and right-arm facing corner chair
+"Left-arm" and "right-arm" describe the position of the arm when you face the piece
+Corner-block frame
+Loose feather-blend cushions
+2 toss pillows included
+Pillows with feather-blend inserts
+Polyester upholstery and pillows
+Exposed feet with faux wood finish
+Estimated Assembly Time: 10 Minutes
+Fabric Details
+Body: Polyester (100)%
+Body and Pillows: Polyester (100)%
+Weight
+215 lbs. (96.75 kgs.)", category: "Couch and Sofa", measurement: 'Width: 112.00"Depth: 40.00"Height: 37.00"', color: "Ivory, Dark Gray ", price: 1599.00 })
+
+Product.create({name: "Alcona Sofa", description: "Whether you’re creating a warm and cozy modern farmhouse or cool and contemporary escape, the Alcona sofa is sure to look right at home. High armrests with a rounded track design provide a chic, sheltering effect. Light linen-tone upholstery and 3-over-2 cushion styling is a beauty to behold.
+Corner-blocked frame
+Attached back and reversible seat cushions
+High-resiliency foam cushions wrapped in thick poly fiber
+Platform foundation system resists sagging 3x better than spring system after 20,000 testing cycles by providing more even support
+Smooth platform foundation maintains tight, wrinkle-free look without dips or sags that can occur over time with sinuous spring foundations
+5 toss pillows included
+Pillows with soft polyfill
+Polyester upholstery; polyester, polyester/linen, polyester/rayon pillows
+Exposed feet with faux wood finish
+Fabric Details
+Body and Oneside Toss and Kidney: Polyester (100)%
+Ottoman Top: Polyester (100)%
+Oneside Kidney Pillow: Polyester (91)%,Rayon (9)%
+Toss Pillows: Linen (9)%,Polyester (91)%
+Weight
+148 lbs. (67.13 kgs.)", category: "Couch and Sofa", measurement: 'Width: 95.00"Depth: 40.00"Height: 40.00"', color: "Linen", price: 949.00})
+Product.create({name: "", description: "", category: "Couch and Sofa", measurement: '', color: "", price: })
+Product.create({name: "", description: "", category: "Couch and Sofa", measurement: '', color: "", price: })
