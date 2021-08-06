3a211fe436eca5fd1204db91750f3c80006d84d9
Yun Sung Choi <cyss0317@gmail.com>
2021-08-06 14:09:20 -0500

    CSS added

--- frontend/components/products/product_Index.jsx
+++ frontend/components/products/product_Index.jsx
@@ -15,6 +15,7 @@ class ProductIndex extends React.Component{
 
         return (
             <div className="products-index-container" >
+                <h2 className="index-container-logo">Furnitures made just for you.</h2>
                 <p className="best-selling-items">Best selling items</p>
                 <ul className="products-index-container-ul">
                         {
