19ba5922baf2d09b95dbb5f0e873ff9382f63440
Yun Sung Choi <cyss0317@gmail.com>
2021-08-07 16:24:43 -0500

    Add nav bars in index page

--- frontend/components/products/product_Index.jsx
+++ frontend/components/products/product_Index.jsx
@@ -12,21 +12,43 @@ class ProductIndex extends React.Component{
 
     render(){
         const { products, couchAndSofa } = this.props;
+        const categories = ["Couch and Sofa",
+            "Console Table",
+            "Outdoor Dining Sets",
+            "Dining Table",
+            "Coffee Table",
+            "Kids Bunk Bed",
+            "Accent Chairs",
+            "Dining Chairs",
+            "Outdoor"]
 
         return (
-            <div className="products-index-container" >
-                <h2 className="index-container-logo">Furnitures made just for you.</h2>
-                <p className="best-selling-items">Best selling items</p>
-                <ul className="products-index-container-ul">
-                        {
-                        couchAndSofa.map(product => (       
-                            <ProductIndexItem
-                                product={product}
-                                key={product.id}
-                            />
-                           )
-                        )}
-                </ul>
+            <div className="index-main-container">
+                <nav className="nav-container">
+                    {
+                        categories.map((category) => (
+                            <a className="nav-item">
+                                {category}
+                            </a>
+                        ))
+                    }
+                </nav>
+                <div className="products-index-container" >
+                    <h2 className="index-container-logo">Furnitures made just for you.</h2>
+                    <p className="best-selling-items">Best selling items</p>
+                    <ul className="products-index-container-ul">
+                            {
+                            couchAndSofa.map(product => (       
+                                <ProductIndexItem
+                                    product={product}
+                                    key={product.id}
+                                />
+                            )
+                            )}
+                    </ul>
+                </div>
+                
+
             </div>
         )
     }
