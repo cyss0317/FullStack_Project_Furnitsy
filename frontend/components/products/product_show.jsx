bf0ec9afcb21b563726a9d78079801772993c580
Yun Sung Choi <cyss0317@gmail.com>
2021-08-06 19:12:54 -0500

    Add CSS to show

--- frontend/components/products/product_show.jsx
+++ frontend/components/products/product_show.jsx
@@ -54,26 +54,27 @@ class ProductShow extends React.Component {
                     <p className="show-measurement">Measurement:  {product.measurement}</p>
                 </div>
                 
-                <div className="product-show-picture-container">
-                    <div className="main-picture-container">
-                        <button className="svg-button">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M16,21a0.994,0.994,0,0,1-.664-0.253L5.5,12l9.841-8.747a1,1,0,0,1,1.328,1.494L8.5,12l8.159,7.253A1,1,0,0,1,16,21Z"></path></svg>
-                        </button>
-                        <img className="picture-container-main"src={ this.state.mainPic === undefined ? this.props.photoUrls[0] : this.state.mainPic } alt="" />
-                        <button className="svg-button">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M8,21a1,1,0,0,1-.664-1.747L15.5,12,7.336,4.747A1,1,0,0,1,8.664,3.253L18.5,12,8.664,20.747A0.994,0.994,0,0,1,8,21Z"></path></svg>
-                        </button>
+                <div className="product-show-picture-container-outter">
+                    <div className="product-show-picture-container-inner" >
+                        <div className="main-picture-container">
+                            <button className="svg-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M16,21a0.994,0.994,0,0,1-.664-0.253L5.5,12l9.841-8.747a1,1,0,0,1,1.328,1.494L8.5,12l8.159,7.253A1,1,0,0,1,16,21Z"></path></svg>
+                            </button>
+                            <img className="picture-container-main"src={ this.state.mainPic === undefined ? this.props.photoUrls[0] : this.state.mainPic } alt="" />
+                            <button className="svg-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M8,21a1,1,0,0,1-.664-1.747L15.5,12,7.336,4.747A1,1,0,0,1,8.664,3.253L18.5,12,8.664,20.747A0.994,0.994,0,0,1,8,21Z"></path></svg>
+                            </button>
+                        </div>
+                        <ul className="picture-container-list">
+                            {product.photoUrls.map((photo) => (
+                            <li className="picture-container-list-item-container">
+                                <img className="picture-container-list-item" src={photo} alt={photo} onClick={this.clickPictureHandler}  />
+                            </li>
+                            ))
+                            }
+                        </ul>
                     </div>
-                    <ul className="picture-container-list">
-                        {product.photoUrls.map((photo) => (
-                        <li className="picture-container-list-item-container">
-                            <img className="picture-container-list-item" src={photo} alt={photo} onClick={this.clickPictureHandler}  />
-                        </li>
-                        ))
-                        }
-                    </ul>
                 </div>
-
 
             </section>
         )
