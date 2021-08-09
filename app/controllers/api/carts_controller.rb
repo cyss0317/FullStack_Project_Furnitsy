0da0575e1d42c80a7ed9dacf37fc070a231cef97
Yun Sung Choi <cyss0317@gmail.com>
2021-08-09 18:35:58 -0500

    Carts controller Added

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,2 +1,30 @@
 class Api::CartsController < ApplicationController
+
+    before_action :require_login
+
+    def create
+        @cart = Cart.new(cart_params)
+        if @cart.save
+            render :show
+        else
+            render json: @cart.errors.full_messages, status: 422
+        end
+    end
+
+    # # render the view
+    # def edit
+    #     render :edit
+    # end
+    # #interacting with model
+    # def update
+    #     @cart = Cart.find_by(user_id: current_user.id)
+    #     if @cart
+    # end
+
+
+    private
+    def cart_params 
+        params.require(:cart).permits(:user_id)
+    end
+
 end
