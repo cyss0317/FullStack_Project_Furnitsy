0c7af31bcb54d6fb01ddf24186ab839373a082d4
Yun Sung Choi <cyss0317@gmail.com>
2021-08-09 23:58:53 -0500

    Add util ajax request functions

---
+++ frontend/util/cart_items_api_util.js
@@ -0,0 +1,21 @@
+export const createCartItem = (userId, productId)(
+    $.ajax({
+        method: "POST",
+        url: "/api/cart_items",
+        data: {userId, productId}
+    })
+)
+
+export const receiveCartItems = (userId)(
+    $.ajax({
+        url: '/api/cart_items',
+        data: {userId}
+    })
+)
+
+export const deleteCartItem = (userId, productId)(
+    $.ajax({
+        method: "DELETE",
+        url: `/api/cart_items/${userId}`
+    })
+)
