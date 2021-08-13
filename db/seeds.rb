e35a890183b1223691340196e775f3f4c1790980
Yun Sung Choi <cyss0317@gmail.com>
2021-08-13 10:29:33 -0500

    Fixing heroku with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,17 @@
 # require 'open_uri'
 require 'open-uri'
 
-Product.delete_all
-User.delete_all
-Cart.delete_all
-CartItem.delete_all
+# Product.delete_all
+# User.delete_all
+# Cart.delete_all
+# CartItem.delete_all
 
-User.create({email: "demo_user", password: "password", first_name:"DEMO_USER"})
+# User.create({email: "demo_user", password: "password", first_name:"DEMO_USER"})
 
-require_relative './seeds/01_couches.rb'
-require_relative './seeds/02_tables.rb'
-require_relative './seeds/03_beds.rb'
-require_relative './seeds/04_chairs.rb'
+# # require_relative './seeds/01_couches.rb'
+# require_relative './seeds/02_tables.rb'
+# require_relative './seeds/03_beds.rb'
+# require_relative './seeds/04_chairs.rb'
 
 
 # category: "Couch and Sofa"
