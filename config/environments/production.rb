5918e9c61cd2112840005d7d44e7f33a09a1ba76
Yun Sung Choi <cyss0317@gmail.com>
2021-08-25 10:22:05 -0500

    Config files are updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-
   config.assets.js_compressor = Uglifier.new(:harmony => true)
-
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -41,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon_prod
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -63,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Sunny_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "Animezon_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
