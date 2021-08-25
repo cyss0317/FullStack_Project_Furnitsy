cd002872539f87dae0616cde03451c2098997c70
Yun Sung Choi <cyss0317@gmail.com>
2021-08-24 23:19:40 -0500

    changed seeds files and changed config folder to old one

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-#autoformat keys of state from Ruby snake_case to JS camelCase
-Jbuilder.key_format camelize: :lower
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # require 'open_uri'
-# require 'open-uri'
-# require 'csv'
+require 'open-uri'
+require 'csv'
 
-# Product.delete_all
-# User.delete_all
-# Cart.delete_all
-# CartItem.delete_all
+Product.delete_all
+User.delete_all
+Cart.delete_all
+CartItem.delete_all
 
-# User.create({email: "demo_user", password: "password", first_name:"DEMO_USER"})
+User.create({email: "demo_user", password: "password", first_name:"DEMO_USER"})
 
-# require_relative './seeds/01_couches.rb'
-# require_relative './seeds/02_tables.rb'
-# require_relative './seeds/03_beds.rb'
-# require_relative './seeds/04_chairs.rb'
+require_relative './seeds/01_couches.rb'
+require_relative './seeds/02_tables.rb'
+require_relative './seeds/03_beds.rb'
+require_relative './seeds/04_chairs.rb'
 
 
 # category: "Couch and Sofa"
