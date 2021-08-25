72b424c97b0ef2b6163520aad72ac5587e1559af
Yun Sung Choi <cyss0317@gmail.com>
2021-08-24 20:17:04 -0500

    open_uri

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'open_uri'
+require 'open-uri'
 Product.delete_all
 User.delete_all
 Cart.delete_all
