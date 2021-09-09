9a1914600678f93d140f5575ee99922c41f6f9ce
Yun Sung Choi <cyss0317@gmail.com>
2021-09-09 13:26:03 -0500

    Search bar filtering

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductNavShowList from "../products/product_nav_show_list";
+import {Link} from "react-router-dom"
 
 class SearchBar extends React.Component {
     constructor(props){
@@ -22,53 +23,75 @@ class SearchBar extends React.Component {
         // const keyword = new URLSearchParams(window.location).get("s")
         if (products === undefined) return null;
         let keywords = keyword.split(" ")
-        let filteredProducts = products.filter((product) => {
-            // keywords.forEach((word) => {
-                return product.name.toLowerCase().includes(keyword.toLowerCase())
-        //     })
+        let filteredProducts = [];
+        products.forEach((product) => {
+
+            keywords.forEach((word) => {
+                let byName = product.name.toLowerCase().includes(word.toLowerCase())
+                let byColor = product.color.toLowerCase().includes(word.toLowerCase())
+
+                if(byName && !filteredProducts.includes(byName)){
+                    filteredProducts.push(product)
+                } else if (byColor && !filteredProducts.includes(byColor)){
+                    filteredProducts.push(product)
+                } else {
+                    ""
+                }
+                
+            })
+            return filteredProducts;
         })
-        debugger
-
+        // debugger
+        
         return (
-
+        filteredProducts.length > 0 ?
+        <section>
             <div>
-              
-                <div>
+                <label htmlFor="">by Color
+                <input type="radio" /></label>
+            </div>
+            <div>
+                <div className="nav-static">
                     {
                         filteredProducts.map((product) => {
-                           return(
-                               <ProductNavShowList product={product} />
-
-                           )
+                        return(
+                            <ProductNavShowList product={product} />
+                        )
                         })
                     }
-
-                    hellooooooo
+                </div>
+            </div>
+        </section>
+                :
+                <div>
+                    <Link to="/">There are no products matching your search keywords</Link>
                 </div>
 
-            </div>
-            // <form action="/" method="get" className="search">
-            //     <div className="searchInputs">
-            //         <input type="text" placeholder={placeholder}/>
-            //         <div className="serchIcon"></div>
-            //     </div>
-            //     <div className="dataResult"></div>
-            // </form>
+
+                
+                // <form action="/" method="get" className="search">
+                //     <div className="searchInputs">
+                //         <input type="text" placeholder={placeholder}/>
+                //         <div className="serchIcon"></div>
+                //     </div>
+                //     <div className="dataResult"></div>
+                // </form>
 
 
-            // <form action="/" method="get">
-            //     <label htmlFor="header-search">
-            //         <span className="visually-hidden">Search products</span>
-            //     </label>
-            //     <input
-            //         type="text"
-            //         id="header-search"
-            //         placeholder="Search blog posts"
-            //         name="s"
-            //     />
-            //     <button type="submit">Search</button>
-            // </form>
-        )   
+                // <form action="/" method="get">
+                //     <label htmlFor="header-search">
+                //         <span className="visually-hidden">Search products</span>
+                //     </label>
+                //     <input
+                //         type="text"
+                //         id="header-search"
+                //         placeholder="Search blog posts"
+                //         name="s"
+                //     />
+                //     <button type="submit">Search</button>
+                // </form>
+            )   
+
     }
 }
 export default SearchBar;
