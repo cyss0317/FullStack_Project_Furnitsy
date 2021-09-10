e1112f6109a2bd86ed2f76f5fd246832565ba280
Yun Sung Choi <cyss0317@gmail.com>
2021-09-10 17:26:11 -0500

    fixing issues

--- frontend/components/products/product_show.jsx
+++ frontend/components/products/product_show.jsx
@@ -47,6 +47,7 @@ class ProductShow extends React.Component {
         // }
 
         // important!!!! need to update both states for both click events
+        console.log("hello")
         this.setState({ index: this.props.photoUrls.indexOf(this.state.mainPic) + 1 > this.props.photoUrls.length + 1 ? 0 : this.props.photoUrls.indexOf(this.state.mainPic) + 1})
         this.setState({ mainPic: this.props.photoUrls[this.state.index]})
         // this.setState({ mainPic: this.state.mainPic === undefined ? this.props.photoUrls[0] : this.props.photoUrls[this.props.photoUrls.indexOf(mainPic) + 1] })
@@ -187,7 +188,7 @@ class ProductShow extends React.Component {
                                 </button>
                                 <img className="picture-container-main"src={ this.state.mainPic === undefined ? this.props.photoUrls[0] : this.state.mainPic } alt="" />
                                 <button className="svg-button" id="right-svg-button" onClick={this.rightSvgClickhandler}>
-                                    <svg xmlns="http://www.w3.org/2000/svg"  id="right-svg-button" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M8,21a1,1,0,0,1-.664-1.747L15.5,12,7.336,4.747A1,1,0,0,1,8.664,3.253L18.5,12,8.664,20.747A0.994,0.994,0,0,1,8,21Z"></path></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" onClick={this.rightSvgClickhandler}  id="right-svg-button" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M8,21a1,1,0,0,1-.664-1.747L15.5,12,7.336,4.747A1,1,0,0,1,8.664,3.253L18.5,12,8.664,20.747A0.994,0.994,0,0,1,8,21Z"></path></svg>
                                 </button>
                             </div>
                             <ul className="picture-container-list">
