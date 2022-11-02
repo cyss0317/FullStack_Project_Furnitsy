ff0bb6817d96a4b01db91b65344fdffffb3273f3
Yun Choi <cyss0317@gmail.com>
2022-11-01 23:10:14 -0500

    Created a Footer component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import GreetingContainer from "./greeting_container";
 import LoginFormContainer from "./login_form_container";
 import SignupFormContainer from "./signup_form_container";
+import Footer from "./Footer";
 import { Route, Switch, Link } from "react-router";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import Modal from "./modal";
@@ -73,71 +74,7 @@ const App = () => {
         <AuthRoute path="/login" component={LoginFormContainer} />
         <AuthRoute path="/signup" component={SignupFormContainer} />
       </Switch>
-      <footer>
-        <div className="what_is_furnitsy">
-          <div>
-            <h1>What is Furnitsy?</h1>
-          </div>
-          <div id="what_is_furnitsy_inner">
-            <div id="furnitsy_inner_div">
-              <h2>A community doing good</h2>
-              <p>
-                Furnitsy is a furniture store, where my mother in law buys and
-                sells Ashely furnitures.{" "}
-              </p>
-            </div>
-            <div id="furnitsy_inner_div">
-              <h2>Sell furnitures to customers</h2>
-              <p>
-                We sell Ashely furnitures on our website. Whatever you can find
-                on this website, you can also find it on actual Ashley
-                furnitures.
-              </p>
-            </div>
-            <div id="furnitsy_inner_div">
-              <h2>Peace of mind</h2>
-              <p>
-                Your privacy is the highest priority of our dedicated team. And
-                if you ever need assistance, we are always ready to step in for
-                support.
-              </p>
-            </div>
-          </div>
-          <div>
-            <a
-              id="linked-in"
-              href="https://www.linkedin.com/in/yun-sung-choi-936142214/"
-              target="_blank"
-            >
-              <img
-                src="https://furnitsy-furniture-images.s3.us-east-2.amazonaws.com/LI-Logo.png"
-                alt=""
-                width="160"
-                height="50"
-              />
-            </a>
-            <a id="github" href="https://github.com/cyss0317" target="_blank">
-              <img
-                src="https://furnitsy-furniture-images.s3.us-east-2.amazonaws.com/Octocat.png"
-                alt=""
-                width="100"
-                height="80"
-              />
-            </a>
-            <form
-              action="mailto:cyss0317@gmail.com"
-              method="POST"
-              encType="text/plain"
-            >
-              <input
-                className="submit_to_email"
-                type="submit"
-                value="Contact Yun Sung Choi"
-              />
-            </form>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
