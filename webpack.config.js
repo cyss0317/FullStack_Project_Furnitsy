77ecea210fbeacfd831e28d9ffa901233c8a3dd0
Yun Choi <cyss0317@gmail.com>
2022-11-26 00:57:40 -0600

    Updated config

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/env", "@babel/react"],
+            // presets: ["@babel/env", "@babel/react"],
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
       },
