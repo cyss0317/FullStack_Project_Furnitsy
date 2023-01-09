01b75494d406275c0eb1dd5ace6d113ae6818630
Yun Choi <cyss0317@gmail.com>
2023-01-09 08:54:28 -0600

    Edited CSS

--- frontend/components/carts/cart_show.jsx
+++ frontend/components/carts/cart_show.jsx
@@ -51,7 +51,7 @@ class CartShow extends React.Component {
 
     if (numberOfProducts === 0) {
       return (
-        <div className="margin3">
+        <div className="margin6vw">
           <h1>There is nothing in your cart</h1>
           <Link to="/">Continue shopping?</Link>
         </div>
