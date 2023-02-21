93f55a2df164d82cf8b68a7f3e57075a6744e186
Yun Choi <cyss0317@gmail.com>
2023-02-20 21:58:36 -0600

    Greeting.tsx jest

---
+++ frontend/components/Greeting.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import Greeting from "./Greeting";
+import 
+describe("Greeting", () => {
+  it("renders correctly", () => {
+    user = 
+    render(<Greeting currentUser={} />);
+  })
+})
